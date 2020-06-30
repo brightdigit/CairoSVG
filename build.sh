fd9204e1764344a6c1384a743276abf02e6cc519
Leo Dion <leogdion@brightdigit.com>
2020-06-29 20:36:28 -0400

    working build and xcodegen

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 rm -rf Dependencies
 mkdir -p Dependencies
 libraries=(lzo cairo fontconfig freetype fribidi gdk-pixbuf gettext glib graphite2 harfbuzz icu4c libcroco libffi libpng librsvg pango pcre pixman)
@@ -8,8 +10,39 @@ do
    # do whatever on $i
 done
 
+DEPFILE="$(mktemp)"
+CPFILE="$(mktemp)"
+
 echo "targets:" > dependencies.yml
 echo "  CairoSVG:" >> dependencies.yml
-echo "    dependencies:" >> dependencies.yml
-find Dependencies -type f -name '*.dylib' | xargs -I {} echo "    - sdk: \"$(SRCROOT)/{}\"" >> dependencies.yml
+
+
+ dylibs=()
+ while IFS= read -d $'\0' -r file ; do
+     dylibs=("${dylibs[@]}" "$file")
+ done < <(find Dependencies -type f -name '*.dylib' -print0)
+
+touch $DEPFILE
+touch $CPFILE
+
+echo "    dependencies:" > $DEPFILE
+echo "    sources:" > $CPFILE
+
+for dependency in "${dylibs[@]}"
+do 
+ echo "    - sdk: \"$dependency\"" >> $DEPFILE
+ echo "      root: SOURCE_ROOT" >> $DEPFILE
+ echo "      embed: true" >> $DEPFILE
+ echo "      implicit: true" >> $DEPFILE
+ echo "      codeSign: true" >> $DEPFILE
+
+ echo "         - path: \"$dependency\"" >> $CPFILE
+ echo "           buildPhase:" >> $CPFILE
+ echo "             copyFiles:" >> $CPFILE
+ echo "               destination: frameworks" >> $CPFILE
+ echo "               codeSign: true" >> $CPFILE
+done
+
+cat $DEPFILE >> dependencies.yml
+cat $CPFILE >> dependencies.yml
 xcodegen
