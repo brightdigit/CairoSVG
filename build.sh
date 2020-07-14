43c51cdd8ca2f56f853eb9e69270ab34972dfcc3
Leo Dion <leogdion@brightdigit.com>
2020-07-14 09:48:59 -0400

    fixed issue with code signing

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+rm -rf build 
+mkdir build
 xcodebuild archive -scheme CairoSVG -configuration Release -archivePath ./build/CairoSVG.xcarchive SKIP_INSTALL=NO -arch x86_64
 xcodebuild -create-xcframework -framework ./build/CairoSVG.xcarchive/Products/Library/Frameworks/CairoSVG.framework -output ./build/CairoSVG.xcframework
