6db16fce5a932580c61a927eb23d5ace98d8189c
Leo Dion <leogdion@brightdigit.com>
2020-07-17 15:39:22 -0400

    added compute-checksum and ditto

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ mkdir build
 xcodebuild test -scheme CairoSVGTests
 xcodebuild archive -scheme CairoSVG -configuration Release -archivePath ./build/CairoSVG.xcarchive SKIP_INSTALL=NO -arch x86_64
 xcodebuild -create-xcframework -framework ./build/CairoSVG.xcarchive/Products/Library/Frameworks/CairoSVG.framework -output ./build/CairoSVG.xcframework
+ditto -c -k --sequesterRsrc --keepParent build/CairoSVG.xcframework build/CairoSVG.xcframework.zip
+swift package compute-checksum build/CairoSVG.xcframework.zip
