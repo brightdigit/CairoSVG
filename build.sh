965298c867a6cb9f43d7835d64de2e00e63767c8
Leo Dion <leogdion@brightdigit.com>
2020-07-17 14:46:43 -0400

    first ci build

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf build 
 mkdir build
+xcodebuild test -scheme CairoSVGTests
 xcodebuild archive -scheme CairoSVG -configuration Release -archivePath ./build/CairoSVG.xcarchive SKIP_INSTALL=NO -arch x86_64
 xcodebuild -create-xcframework -framework ./build/CairoSVG.xcarchive/Products/Library/Frameworks/CairoSVG.framework -output ./build/CairoSVG.xcframework
