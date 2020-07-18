c4a612d0ce7727dcd1f2db30eb156afbe07ba6b1
GitHub Action <action@github.com>
2020-07-18 00:46:48 +0000

    checksum update:0.1.1-

--- Package.swift
+++ Package.swift
@@ -17,9 +17,8 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .binaryTarget(
       name: "CairoSVG",
-      path: "build/CairoSVG.xcframework"
-      // url: "https://github.com/brightdigit/CairoSVG/releases/download/0.1.0-alpha.1/CairoSVG.xcframework.zip",
-      // checksum: "3a1f7ba992691e7c4b191c79f8e8cb4318d61811fc47dd0e09314c321fac023a"
+      url: "https://github.com/brightdigit/CairoSVG/releases/download/0.1.1/CairoSVG.xcframework.zip",
+      checksum: ""
     )
   ]
 )
